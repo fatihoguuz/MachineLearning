648971c52776b22d68b1b40715a46caff3f79275
Fatih Oguz <oguzfatih39@gmail.com>
2024-05-01 10:58:51 +0300

    Initial Commit

---
+++ MachineLearning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineLearning
+//
+//  Created by Fatih Oğuz on 1.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
