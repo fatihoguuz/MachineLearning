a73d1b472ba9c55a4fde97fe26e7327eebe5c26b
Fatih Oguz <oguzfatih39@gmail.com>
2024-05-01 22:08:41 +0300

    update

--- MachineLearning/ViewController.swift
+++ MachineLearning/ViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var resultLabel: UILabel!
+    var chosenImage = CIImage()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +30,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,UINaviga
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imageView.image = info[.originalImage] as? UIImage
         self.dismiss(animated: true, completion: nil)
+        
+        if let ciImage = CIImage(image: imageView.image!){
+            chosenImage = ciImage
+        }
+        recognizeImage(image: chosenImage)
+    }
+    func recognizeImage(image: CIImage){
+        
     }
 }
 
